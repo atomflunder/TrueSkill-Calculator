6291c493f75ee6b04a3a07b24f34dc8cc2900781
atomflunder <80397293+atomflunder@users.noreply.github.com>
2025-05-17 20:01:57 +0200

    Enhance separator with label

--- components/ui/separator/Separator.vue
+++ components/ui/separator/Separator.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
-import { reactiveOmit } from '@vueuse/core'
-import { Separator, type SeparatorProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue';
+import { reactiveOmit } from '@vueuse/core';
+import { Separator, type SeparatorProps } from 'reka-ui';
+import { cn } from '@/lib/utils';
 
-const props = withDefaults(defineProps<
-  SeparatorProps & { class?: HTMLAttributes['class'] }
->(), {
-  orientation: 'horizontal',
-  decorative: true,
-})
+const props = defineProps<
+    SeparatorProps & { class?: HTMLAttributes['class']; label?: string }
+>();
 
-const delegatedProps = reactiveOmit(props, 'class')
+const delegatedProps = reactiveOmit(props, 'class');
 </script>
 
 <template>
-  <Separator
-    data-slot="separator-root"
-    v-bind="delegatedProps"
-    :class="
-      cn(
-        `bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px`,
-        props.class,
-      )
-    "
-  />
+    <Separator
+        v-bind="delegatedProps"
+        :class="
+            cn(
+                'shrink-0 bg-border relative',
+                props.orientation === 'vertical'
+                    ? 'w-px h-full'
+                    : 'h-px w-full',
+                props.class
+            )
+        "
+    >
+        <span
+            v-if="props.label"
+            :class="
+                cn(
+                    'text-md text-muted-foreground bg-background absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center',
+                    props.orientation === 'vertical'
+                        ? 'w-[1px] px-1 py-2'
+                        : 'h-[1px] py-1 px-2'
+                )
+            "
+            >{{ props.label }}</span
+        >
+    </Separator>
 </template>
